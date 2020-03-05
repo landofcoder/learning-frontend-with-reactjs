8912e2b61c7adc6d07a789bc2ba7ed5a3aa35471
chxnhung <ozejang1998@gmail.com>
2020-03-05 17:39:05 +0700

    up

--- tranning-react-Hung/src/Components/body/Container/Container.js
+++ tranning-react-Hung/src/Components/body/Container/Container.js
@@ -2,94 +2,156 @@ import React, {Component} from 'react';
 import image from "./search.png";
 import './Container.scss'
 
+const defaultList = [
+    {
+        id: 1,
+        name: 'Jone',
+        age: 20,
+        email: 'jone67@gmail.com',
+        address: 'London'
+    },
+    {
+        id: 2,
+        name: 'Emma',
+        age: 25,
+        email: 'prettyemma@gmail.com',
+        address: 'USA'
+    },
+    {
+        id: 3,
+        name: 'Frank',
+        age: 29,
+        email: 'shameless@gmail.com',
+        address: 'USA'
+    }];
 
 class Container extends Component {
     constructor(props) {
         super(props);
         this.state = {
             value: '',
-            item: {},
-            list: [
-                {
-                    id: 1,
-                    name: 'Jone',
-                    age: 20,
-                    email: 'jone67@gmail.com',
-                    address: 'London'
-                },
-                {
-                    id: 2,
-                    name: 'Emma',
-                    age: 20,
-                    email: 'prettyemma@gmail.com',
-                    address: 'USA'
-                },
-                {
-                    id: 3,
-                    name: 'Frank',
-                    age: 20,
-                    email: 'shameless@gmail.com',
-                    address: 'USA'
-                }
-            ],
+            id: 0,
+            age: 0,
+            name: '',
+            email: '',
+            address: '',
+            listDefault: defaultList,
+            list: defaultList,
         };
     }
-    onRemoveItem = ((id) => {
-        this.setState(state => {
-            const list = state.list.filter(item => item.id !== id);
-            return {
-                list,
-            };
-        });
-    });
+
     onChangeValue = event => {
-        this.setState({ value: event.target.value });
+        this.setState({value: event.target.value});
     };
-    onChange = (event) => {
-        // lay ra prop "name" va value cua the input
-        const {name, value} = event.target;
-        // luu cac value vao state.item
-        this.setState(state => {
-            return(
-                {...state, item: {...state.item, [name]: value}}
-                );
-        });
-    }
+
+
     onChangeId = event => {
-        this.setState({ id: event.target.id });
+        const {name, value} = event.target;
+        this.setState({id: event.target.value})
     };
-
-
-
+    onChangeName = event => {
+        const {name, value} = event.target;
+        this.setState({name: event.target.value});
+    };
+    onChangeAge = event => {
+        const {name, value} = event.target;
+        this.setState({age: event.target.value});
+    };
+    onChangeEmail = event => {
+        const {name, value} = event.target;
+        this.setState({email: event.target.value});
+    };
+    onChangeAddress = event => {
+        const {name, value} = event.target;
+        this.setState({address: event.target.value});
+    };
     onAddItem = () => {
-        let newList = [...this.state.list];
-        newList.push(this.state.item);
-        this.setState(state => {
-            return({...state, list:[...newList], item: {}})
-        });
+        const id = this.state.id;
+        const name = this.state.name;
+        const age = this.state.age;
+        const email = this.state.email;
+        const address = this.state.address;
+        let item = {
+            id: Number(id),
+            name: name,
+            age: Number(age),
+            email: email,
+            address: address
+        };
+        const { list } = this.state;
+        const newList = [...list];
+        let index = 0;
+        for(let i = 0; i < newList.length; i++){
+            if(newList[i].id === id){
+                index = i;
+            }
+        }
+        if(newList[index].id === id){
+            newList.splice(index, 1,item);
+        }else{
+            newList.push(item);
+        }
+        this.setState({list: newList});
     };
-    // onAddItem = () => {
-    //     // copy du lieu tu state.list ra 1 bien moi
-    //     let newList = [...this.state.list];
-    //     // day item đang thao tác và list mới
-    //     newList.push(this.state.item);
-    //     // thay state.list cũ bằng list mới và reset lại state.item
+
+    onUpdateItem = ((id) => {
+       const { list } = this.state;
+       const listUpdate = [...list];
+       console.log(listUpdate);
+       let index = 0;
+       for(let i = 0; i < listUpdate.length; i++){
+           if(listUpdate[i].id === id){
+               index = i;
+           }
+       }
+       let newItem = listUpdate[index];
+       console.log('Item: ', newItem)
+
+       // console.log('listUpdate', listUpdate)
+        this.setState({id: newItem.id,
+            name: newItem.name,
+            age: newItem.age,
+            email: newItem.email,
+            address: newItem.address} );
+    });
+
+
+    onRemoveItem = ((id) => {
+        const { list } = this.state;
+        const listArr = [...list];
+        console.log(listArr);
+        let index = 0;
+        for(let i =0; i < listArr.length; i++){
+            if(listArr[i].id === id )
+            {
+                index = i;
+            }
+        }
+        console.log('index ', index);
+        listArr.splice(index,1);
+        console.log('after listArr:', listArr);
+        this.setState({list: listArr});
+    });
+
+    // onRemoveItem = ((id) => {
     //     this.setState(state => {
-    //         return ({ ...state , list: [...newList],item: {}})
+    //         const list = state.list.filter(item => item.id !== id);
+    //         return {
+    //             list,
+    //         };
     //     });
-    // };
-    // onClearArray = () => {
-    //     this.setState({ students: [] });
-    // };
-    onResetArray = () => {
-        this.setState( state =>{
-            return (state.list)
-        });
+    // });
+
+
+    onReset = () => {
+        this.setState({list: defaultList})
     };
+
+
+
 
     render() {
-        const {list, value} = this.state;
-
+        // const {item} = this.state;
         console.log(this.state);
 
         return (
@@ -149,18 +211,22 @@ class Container extends Component {
                 <div className="content">
 
                     <ul>
-                        {this.state.list.map(item => (
-                            <li key={item.id}>
-                                <strong>ID: </strong> {item.id}
+                        {this.state.list.map(( item, index) => (
+                            <li index={item.id}>
+                                {index.id}<strong>ID: </strong> {item.id}
                                 <br/>
-                                <strong>name: </strong> {item.name}
+                                {index.id}<strong>name: </strong> {item.name}
                                 <br/>
-                                <strong>age: </strong> {item.age} years old.
+                                {index.id}<strong>age: </strong> {item.age} years old.
                                 <br/>
-                                <strong>email :</strong> {item.email}
+                                {index.id}<strong>email :</strong> {item.email}
                                 <br/>
-                                <strong>name :</strong> {item.address}
+                                {index.id}<strong>name :</strong> {item.address}
                                 <br/>
+                                <button type="button" onClick={() => this.onUpdateItem(item.id)}>
+                                    Update
+                                </button>
+
                                 <button
                                     type="button"
                                     onClick={() => this.onRemoveItem(item.id)}
@@ -171,21 +237,22 @@ class Container extends Component {
                         ))}
                     </ul>
                     <form value={this.state.value}
-                          onChange={this.onChangeValue} >
+                          onChange={this.onChangeValue}>
                         <label>
                             id:<br/>
-                            <input type="text" id={this.state.id} name="id" onChange={this.onChange}/>
+                            <input type="text" value={this.state.id} name="id" onChange={this.onChangeId}/>
                             <br/>name:<br/>
-                            <input type="text" name={this.state.name} name="name" onChange={this.onChange}/>
+                            <input type="text" value={this.state.name} name="name" onChange={this.onChangeName}/>
                             <br/>age:<br/>
-                            <input type="text" age={this.state.age} name="age" onChange={this.onChange}/>
+                            <input type="text" value={this.state.age} name="age" onChange={this.onChangeAge}/>
                             <br/>email:<br/>
-                            <input type="text" email={this.state.email} name="email" onChange={this.onChange}/>
+                            <input type="text" value={this.state.email} name="email" onChange={this.onChangeEmail}/>
                             <br/>address: <br/>
-                            <input type="text" address={this.state.address} name="address" onChange={this.onChange}/>
+                            <input type="text" value={this.state.address} name="address" onChange={this.onChangeAddress}/>
                         </label><br/>
                         <br/>
-                        <button type="button" onClick={this.onAddItem} disabled={!this.state.value}>
+
+                        <button type="button" onClick={this.onAddItem} >
                             Add
                         </button>
                     </form>
@@ -193,8 +260,8 @@ class Container extends Component {
                     {/*    Clear Array*/}
                     {/*</button>*/}
                     <br/>
-                    <button type="button" onClick={this.onResetArray}>
-                        Reset Array
+                    <button type="button" onClick={this.onReset}>
+                        Reset
                     </button>
                     <br/>
                     <br/>
